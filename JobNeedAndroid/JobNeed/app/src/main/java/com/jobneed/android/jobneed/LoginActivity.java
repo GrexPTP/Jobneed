b593e84edb998f0e35012c2ce817f64bf1f3225c
Phat PT <phatpt.dev@gmail.com>
2018-12-12 21:08:03 +0700

    Create Login and Signup activities
    Complete designing layout

--- JobNeedAndroid/JobNeed/app/src/main/java/com/jobneed/android/jobneed/LoginActivity.java
+++ JobNeedAndroid/JobNeed/app/src/main/java/com/jobneed/android/jobneed/LoginActivity.java
@@ -1,13 +1,24 @@
 package com.jobneed.android.jobneed;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextView signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        signUp = findViewById(R.id.sign_up);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
